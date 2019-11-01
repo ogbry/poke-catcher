f8ea6f0e5b0d49e5847309ef20a9b885312e78dd
Bryan Alfuente <bryan.alfuente@boom.camp>
2019-11-01 04:08:22 -0400

    update

--- scripts/app.js
+++ scripts/app.js
@@ -127,113 +127,113 @@ $(document).ready(function(){
             }
             
             else{
+                triggerCapture.style.WebkitAnimationName = "myNEWmove";
                 var soundBattle = document.querySelector('.sound-battle')
-            soundBattle.innerHTML =""
-            var audio = document.createElement('audio')
-            audio.setAttribute('autoplay', 'true')
-            var source = document.createElement('source')
-            source.setAttribute('src', 'http://23.237.126.42/ost/pokemon-gameboy-sound-collection/gbhogmtx/107-battle%20%28vs%20wild%20pokemon%29.mp3')
-            source.setAttribute('type', 'audio/ogg')
+                soundBattle.innerHTML =""
+                var audio = document.createElement('audio')
+                audio.setAttribute('autoplay', 'true')
+                var source = document.createElement('source')
+                source.setAttribute('src', 'http://23.237.126.42/ost/pokemon-gameboy-sound-collection/gbhogmtx/107-battle%20%28vs%20wild%20pokemon%29.mp3')
+                source.setAttribute('type', 'audio/ogg')
 
-            audio.appendChild(source)
+                audio.appendChild(source)
 
-            soundBattle.append(audio)
-            $('.intro-bmg').remove()
-            
-            document.querySelector('.capture-bmg').innerHTML =""
-            triggerCapture.style.display = ""
-            document.querySelector('.details').style.borderColor = "lightslategray";
-            getData(`${$('.area-select').val()}`)
-            .then(function(pokeData){
-                appendEncounter.innerHTML= ""
-                detailsBox.innerHTML=""
-                document.querySelector('.tooltiptext').innerHTML = "Capture "
-                var randomNum = Math.floor((Math.random() * pokeData.pokemon_encounters.length -1) + 1)
-
+                soundBattle.append(audio)
+                $('.intro-bmg').remove()
                 
-                getData(`${pokeData.pokemon_encounters[randomNum].pokemon.url}`)
-                .then(function(encPoke){
+                document.querySelector('.capture-bmg').innerHTML =""
+                triggerCapture.style.display = ""
+                document.querySelector('.details').style.borderColor = "lightslategray";
+                getData(`${$('.area-select').val()}`)
+                .then(function(pokeData){
+                    appendEncounter.innerHTML= ""
+                    detailsBox.innerHTML=""
+                    document.querySelector('.tooltiptext').innerHTML = "Capture "
+                    var randomNum = Math.floor((Math.random() * pokeData.pokemon_encounters.length -1) + 1)
+
                     
-                    var stats = [];
-                    encPoke.stats.map(statElem =>{
-                        stats.push(statElem.base_stat)
+                    getData(`${pokeData.pokemon_encounters[randomNum].pokemon.url}`)
+                    .then(function(encPoke){
+                        
+                        var stats = [];
+                        encPoke.stats.map(statElem =>{
+                            stats.push(statElem.base_stat)
+                        })
+                        
+                        var capImage = document.createElement('img')
+                        capImage.setAttribute('class', 'capture-btn')
+                        capImage.setAttribute('data-id', `${encPoke.name}`)
+                        capImage.setAttribute('data-src', `${encPoke.sprites.front_shiny}`)
+                        
+                        capImage.setAttribute('src', `${encPoke.sprites.front_shiny}`)
+                        console.log(stats)
+
+                        var spanDiv = document.createElement('div')
+                        var headerDiv = document.createElement('div')
+
+                        headerDiv.setAttribute('class', 'name-header')
+                        spanDiv.setAttribute('class', 'poke-details')
+
+                        var capSpan = document.createElement('span')
+                        var capSpan1 = document.createElement('span')
+                        var capSpan2 = document.createElement('span')
+                        var capSpan3 = document.createElement('span')
+                        var capSpan4 = document.createElement('span')
+                        var capSpan5 = document.createElement('span')
+                        var capSpanHeader = document.createElement('span')
+                        var pokeName = document.createElement('span')
+
+                        capSpan.innerText += `Speed: ${stats[0]}`
+                        capSpan1.innerText = `Special Defense: ${stats[1]}`
+                        capSpan2.innerText = `Special Attack: ${stats[2]}`
+                        capSpan3.innerText = `Defense: ${stats[3]}`
+                        capSpan4.innerText = `Defense: ${stats[4]}`
+                        capSpan5.innerText = `HP: ${stats[5]}`
+                        pokeName.innerText = `${encPoke.name}`.toUpperCase();
+
+                        capSpanHeader.innerText = `${encPoke.name}`.toUpperCase()
+                        
+
+                        spanDiv.appendChild(capSpan)
+                        spanDiv.appendChild(capSpan1)
+                        spanDiv.appendChild(capSpan2)
+                        spanDiv.appendChild(capSpan3)
+                        spanDiv.appendChild(capSpan4)
+                        spanDiv.appendChild(capSpan5)
+
+                        document.querySelector('.tooltiptext').append(pokeName)
+
+                        headerDiv.append(capSpanHeader)
+                        detailsBox.append(headerDiv)
+                        detailsBox.append(spanDiv)
+                        appendEncounter.append(capImage)
                     })
-                    
-                    var capImage = document.createElement('img')
-                    capImage.setAttribute('class', 'capture-btn')
-                    capImage.setAttribute('data-id', `${encPoke.name}`)
-                    capImage.setAttribute('data-src', `${encPoke.sprites.front_shiny}`)
-                    
-                    capImage.setAttribute('src', `${encPoke.sprites.front_shiny}`)
-                    console.log(stats)
 
-                    var spanDiv = document.createElement('div')
-                    var headerDiv = document.createElement('div')
-
-                    headerDiv.setAttribute('class', 'name-header')
-                    spanDiv.setAttribute('class', 'poke-details')
-
-                    var capSpan = document.createElement('span')
-                    var capSpan1 = document.createElement('span')
-                    var capSpan2 = document.createElement('span')
-                    var capSpan3 = document.createElement('span')
-                    var capSpan4 = document.createElement('span')
-                    var capSpan5 = document.createElement('span')
-                    var capSpanHeader = document.createElement('span')
-                    var pokeName = document.createElement('span')
-
-                    capSpan.innerText += `Speed: ${stats[0]}`
-                    capSpan1.innerText = `Special Defense: ${stats[1]}`
-                    capSpan2.innerText = `Special Attack: ${stats[2]}`
-                    capSpan3.innerText = `Defense: ${stats[3]}`
-                    capSpan4.innerText = `Defense: ${stats[4]}`
-                    capSpan5.innerText = `HP: ${stats[5]}`
-                    pokeName.innerText = `${encPoke.name}`.toUpperCase();
-
-                    capSpanHeader.innerText = `${encPoke.name}`.toUpperCase()
-                    
-
-                    spanDiv.appendChild(capSpan)
-                    spanDiv.appendChild(capSpan1)
-                    spanDiv.appendChild(capSpan2)
-                    spanDiv.appendChild(capSpan3)
-                    spanDiv.appendChild(capSpan4)
-                    spanDiv.appendChild(capSpan5)
-
-                    document.querySelector('.tooltiptext').append(pokeName)
-
-                    headerDiv.append(capSpanHeader)
-                    detailsBox.append(headerDiv)
-                    detailsBox.append(spanDiv)
-                    appendEncounter.append(capImage)
                 })
-
-            })
-            }
+                }
 
             
 
         })
 
         triggerCapture.addEventListener('click', function(){
+            
             triggerCapture.style.display ="none"
             document.querySelector('.capturing').style.display = "flex"
             document.querySelector('.capture-btn').style.display = "none"
-            
+            exploreBtn.style.visibility = "hidden"
             setTimeout(function(){
                 if(pokemonsOwned.length > 5) {
                     alert('Your Pokebag is full')
                 }
                 else{
+                    exploreBtn.style.visibility = ""
                     document.querySelector('.capturing').style.display = "none"
                     triggerCapture.style.display ="none"
                     document.querySelector('.captured').style.borderColor = "lightslategray";
                     document.querySelector('.details').style.borderColor = "transparent";
                     document.querySelector('.tooltiptext').innerHTML = "Capture "
     
-                    console.log($('.capture-btn').data('id'))
-                    console.log($('.capture-btn').data('src'))
-                    
                     var pokeObj = {
                         name: $('.capture-btn').data('id'),
                         imgUrl: $('.capture-btn').data('src')
